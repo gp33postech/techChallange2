c84fc52550c94ccccceb621ad38d393c98ddaa6b
HADSADRIANO <adriano_araujo6@yahoo.com.br>
2025-05-11 09:47:38 -0300

    Ajuste para funcionar local e no render

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
-//Rodar no render
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/api-blog';
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB conectado');
-    } catch (error) {
-        console.error('Erro ao conectar ao MongoDB', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('🟢 Conectado ao MongoDB');
+  } catch (error) {
+    console.error('🔴 Erro ao conectar ao MongoDB', error);
+  }
 };
 
 module.exports = connectDB;
-// module.exports
